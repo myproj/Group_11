3c59be7ffafe65a0cfbae19b4490bc52efec9af8
Sai Tarun Reddy <tarun@Sais-MacBook-Pro.local>
2017-09-03 15:06:53 +0530

    Files at Starting

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include "functions.h"
+
+int factorial(int n)
+{
+	if(n<0)
+	{
+		printf("Enter a positive number\n");
+    	return 0;
+    }
+    
+    if(n>1)
+    {
+		return(n * factorial(n-1));
+    }
+    else 
+        return 1;
+}
