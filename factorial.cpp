6db8725b17f9b873a410a4e9ecffc984efd6a0a4
Sai Tarun reddy <saitarunreddypalla@gmail.com>
2017-09-03 15:22:30 +0530

    Initial commit

--- factorial.cpp
+++ factorial.cpp
@@ -2,16 +2,9 @@
 
 int factorial(int n)
 {
-	if(n<0)
-	{
-		printf("Enter a positive number\n");
-    	return 0;
-    }
-    
     if(n>1)
     {
 		return(n * factorial(n-1));
     }
-    else 
-        return 1;
+    else return 1;
 }
